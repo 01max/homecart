5d7196c78cdd7596c89f08a316e168326816ccb3
01max <m.louguet@gmail.com>
2026-06-14 14:34:52 +0200

    Fix leclerc/paper/v1 parser

--- lib/parser/leclerc/paper/v1.rb
+++ lib/parser/leclerc/paper/v1.rb
@@ -6,8 +6,10 @@ module Parser
         FORMAT = Parser::Registry::FORMATS.fetch(:leclerc_paper_v1)
 
         ITEM_LINE_PATTERN = /\A(?<label>.+?)\s+(?<amount>\d+\.\d{2})\z/
+        DISCOUNT_LINE_PATTERN = /\A(?<label>.+?)\s+(?<amount>-\d+\.\d{2})\z/
+        DETAIL_DISCOUNT_LINE_PATTERN = /\A(?<label>.+?)\s+(?<amount>\d+\.\d{2})\z/
         QUANTITY_LINE_PATTERN = /\A(?<quantity>\d+(?:[,.]\d+)?)\s+X\s+(?<unit_price>\d+\.\d{2})€\s+(?<total>\d+\.\d{2})\z/
-        PAYMENT_PATTERN = /\A(?<raw_label>CB|Bon achat carte)\s+(?<amount>\d+\.\d{2})\z/
+        PAYMENT_PATTERN = /\A(?<raw_label>CB|Bon achat carte|Bon immediat)\s+(?<amount>\d+\.\d{2})\z/
 
         Parser::Registry.register(FORMAT, self)
       end
